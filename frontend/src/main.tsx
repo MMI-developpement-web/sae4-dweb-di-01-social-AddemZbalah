c8627044277b9b5c8317c118d0218ef0bb297d4d
AddemZbalah <addem.zbalah@etu.unilim.fr>
2026-03-20 15:12:06 +0100

    regle probléme hebergement

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -56,7 +56,9 @@ const router = createBrowserRouter([
       },
     ],
   },
-]);
+], {
+  basename: import.meta.env.BASE_URL
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/",
+  base: "/~zbalah3/sae4-dweb-di-01-social-AddemZbalah/frontend/dist/",
   plugins: [react(), tailwindcss()],
   preview: {
    port: 5173,
