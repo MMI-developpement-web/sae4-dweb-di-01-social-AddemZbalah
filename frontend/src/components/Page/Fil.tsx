5ed6a426a1a824f8126a35e17f69785a63168a4d
Addem <addem.zbalah@etu.unilim.fr>
2026-03-26 16:12:36 +0000

    normalement c bon

--- frontend/src/components/Page/Fil.tsx
+++ frontend/src/components/Page/Fil.tsx
@@ -217,7 +217,7 @@ export default function Fil() {
           </svg>
         </button>
         <img
-          src={`${import.meta.env.BASE_URL}assets/image 5 (1).png`}
+          src={`${import.meta.env.BASE_URL}/assets/image 5 (1).png`}
           alt="Logo Zbalah"
           className="pointer-events-auto h-9 w-auto object-contain"
         />
